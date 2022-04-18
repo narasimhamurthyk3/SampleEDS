8ba8c18daa701e56de0baced3a8cca18e6971492
narasimhamurthyk3 <38559055+narasimhamurthyk3@users.noreply.github.com>
2022-04-18 10:58:15 +0530

    Add files via upload

--- ET/java/Example1Controller.java
+++ ET/java/Example1Controller.java
@@ -1,7 +1,5 @@
 package com.cognizant.ecmservice.controllers;
 
-
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,6 +7,7 @@ import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,22 +25,31 @@ public class Example1Controller {
 	// http://localhost:8080/download1?fileName=abc.zip
 	// Using ResponseEntity<InputStreamResource>
 	@RequestMapping("/file/{fileName:.+}")
-	public ResponseEntity<InputStreamResource> downloadFile(@PathVariable("fileName") String fileName) throws IOException {            	
-		MediaType mediaType = getMediaTypeForFileName(this.servletContext, fileName);
-		System.out.println("fileName: " + fileName);
-		System.out.println("mediaType: " + mediaType);
-
-		File file = new File(DIRECTORY + "/" + fileName);
-		InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-
-		return ResponseEntity.ok()
-				// Content-Disposition
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName())
-				// Content-Type
-				.contentType(mediaType)
-				// Contet-Length
-				.contentLength(file.length()) //
-				.body(resource);
+	public ResponseEntity downloadFile(@PathVariable("fileName") String fileName)
+			throws IOException {
+		try {
+			MediaType mediaType = getMediaTypeForFileName(this.servletContext, fileName);
+			System.out.println("fileName: " + fileName);
+			System.out.println("mediaType: " + mediaType);
+			File file = new File(DIRECTORY + "/" + fileName);
+			InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+			System.out.println("File Doesnot exits.....");
+			return ResponseEntity.ok()
+					// Content-Disposition
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName())
+					// Content-Type
+					.contentType(mediaType)
+					// Contet-Length
+					.contentLength(file.length()) //
+					.body(resource);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("EXEPTION:"+e.getMessage());
+			e.printStackTrace(); // see note 2
+			return ResponseEntity
+					.status(HttpStatus.FORBIDDEN)
+					.body(e.getMessage());			
+		}
 	}
 
 	// abc.zip
